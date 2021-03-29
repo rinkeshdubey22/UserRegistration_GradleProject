d0c709560ac4b7a6bddeb0c6ae8cd4cf4a7feb04
Rinkesh Dubey <rinkeshdubey2@gmail.com>
2021-03-29 11:51:51 +0530

    [Rinkesh] : add Added Lambda Function to validate User Entry

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -1,30 +1,37 @@
 package com.bridgelabz;
 
+@FunctionalInterface
+interface lambdaValidationFun {
+    boolean matchPatternLambda(String input, String pattern);
+}
+
 public class UserRegistration {
+
+    lambdaValidationFun dd = (pattern, input) ->  pattern.matches(input);
 
     public boolean isFirstNameValid(String name) {
         String regex = "^([A-Z]{1})([a-z]{2,})$";
-        return name.matches(regex);
+        return dd.matchPatternLambda(name,regex);
     }
 
     public boolean isLastNameValid(String name) {
         String regex = "^([A-Z]{1})([a-z]{2,})$";
-        return name.matches(regex);
+        return dd.matchPatternLambda(name,regex);
     }
 
     public boolean isEmailValid(String name) {
         String regex = "^[a-zA-Z0-9]+([.+-_][a-zA-Z0-9]+)*@[a-zA-z0-9]+.[a-zA-Z]{2}([.][a-zA-Z]{2,4}){0,1}$";
-        return  name.matches(regex);
+        return dd.matchPatternLambda(name,regex);
     }
 
     public boolean isMobileNoValid(String name) {
         String regex = "^([0-9]{2})[ ]([1-9]{1}[0-9]{9})";
-        return  name.matches(regex);
+        return dd.matchPatternLambda(name,regex);
     }
 
     public boolean isPasswordValid(String name) {
         String regex = "^[A-Z]{1}+[a-z]{4,}+[0-9]{1,}+[@$!%*#?&]{1,}";
-        return  name.matches(regex);
+        return dd.matchPatternLambda(name,regex);
     }
 
     public String isEntryValid(boolean validate) {
